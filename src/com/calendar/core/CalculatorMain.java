bb85fae467d5e231d5349ca48f6ff11d074461d8
tradhakrishn1 <tradha.krish1@gmail.com>
2017-02-17 14:02:16 -0600

    Alternate Logic
    Alternate Logic with Streams

--- src/com/calendar/core/CalculatorMain.java
+++ src/com/calendar/core/CalculatorMain.java
@@ -3,6 +3,7 @@
  * Date:02/09/2017
  * Description:This is main class in the library which takes the arithmetic expression from console
  * and prints result on to the console.
+ * Last Edit:2/14/2017
  */
 
 package com.calendar.core;
@@ -16,21 +17,20 @@ public class CalculatorMain {
 			Scanner sc = new Scanner(System.in);
 			try {
 				BasicCalculatorImpl basicCalculatorImpl = new BasicCalculatorImpl(new OperatorPriority(), sc.nextLine());
-				System.out.println("The resulting value of the expression is:"+" "+basicCalculatorImpl.evaluate());
-				System.out.println("Enter true to try on, false to exit");
+				System.out.println("The evaluated result of the expression is:"+" "+basicCalculatorImpl.evaluate());
+				System.out.println("Enter true to continue with one more input, false to exit");
 				oneMoreInput = sc.nextBoolean();
 
 			} catch (WrongFormatExpression wf) {
-
 				System.out.println(
-						"Make sure to have numbers, +,-,*,/% and right order.Enter true to try again, false to exit");
+						"Make sure the expression has numbers, +,-,*,/,% and right arithemetic order.");
+				System.out.println("Enter true to continue with one more input, false to exit");
 				oneMoreInput = sc.nextBoolean();
 			} catch (UnsupportedOperationException uoe) {
 				System.out.println("Make sure to check if there is any divide by zero operation");
-				System.out.println("Enter true to try on, false to exit");
+				System.out.println("Enter true to continue with one more input, false to exit");
 				oneMoreInput = sc.nextBoolean();
 			} catch (Exception e) {
-				System.out.println("General Exception ");
 			}
 		}
 
--- src/com/calendar/core/OperationsUtilities.java
+++ src/com/calendar/core/OperationsUtilities.java
@@ -3,40 +3,32 @@
  * Date:02/09/2017
  * Description:This is a utility class which can be used by other Calculator implementation classes to load operands 
  * ,operators and validate the expression entered.
+ * Last Edit:2/14/2017
  */
 
 package com.calendar.core;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class OperationsUtilities {
 	final static String REGEX = "([0-9]*[+-/%*]?[0-9]*)*";
-	private static List<Double> operands = null;
-	private static List<Character> operators = null;
+	final static String REGEX_STRING_SPLIT = "[+-/%*]";
+	final static List<String> ALLOWED_OPERATORS = Arrays.asList("+", "-", "*", "/", "%");
+	private static List<Double> operands;
+	private static List<Character> operators;
 
 	static void loadOperandsandOperators(String expressionToBeProcessed) {
 		// Function for storing the operands and operators from the input
-		// expression
-		operands = new ArrayList<Double>();
-		operators = new ArrayList<Character>();
-		for (int i = 0; i < expressionToBeProcessed.length(); i++) {
-			String numPart = "";
-			while ((i < expressionToBeProcessed.length()) && (Character.isDigit(expressionToBeProcessed.charAt(i)))) {
-				numPart += expressionToBeProcessed.charAt(i);
-				i++;
-			}
-			if (!numPart.isEmpty())
-				operands.add(new Double(numPart));
-			// Storing the operands on to Array List variable operands
-			if (i < expressionToBeProcessed.length()) {
-				operators.add(expressionToBeProcessed.charAt(i));
-				// Storing the operator symbols on to Array List operators
-				// variable
-			}
-		}
+				operands = Stream.of(expressionToBeProcessed.split(REGEX_STRING_SPLIT)).filter(x -> !x.isEmpty())
+						.map(x -> Double.parseDouble(x)).collect(Collectors.toList());
+				operators = Stream.of(expressionToBeProcessed.split("")).filter(x -> ALLOWED_OPERATORS.contains(x))
+						.map(x -> x.charAt(0)).collect(Collectors.toList());
 	}
 
 	public static ArrayList<Character> loadOperators() {
